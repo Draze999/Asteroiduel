72319099aea8684b7700fc310a9a28e0ebcded46
Aurelien Masset <aurelien.masset@epitech.eu>
2023-12-02 14:03:58 +0100

    Added player rotations

--- player_1.gd
+++ player_1.gd
@@ -6,6 +6,7 @@ var sprite:Sprite2D
 var posX = 0
 var posY = 0
 var Moving
+var distance = 0
 var rotations
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
@@ -20,8 +21,10 @@ func _physics_process(_delta):
 	var directionX = Input.get_axis("MoveLeftP1", "MoveRightP1")
 	var directionY = Input.get_axis("MoveUpP1", "MoveDownP1")
 	
-	rotations = atan2(directionY, directionX) - rotation + PI/2
-	rotate(rotations)
+	distance = sqrt((directionX * directionX) + (directionY * directionY))
+	if (distance > 0.2):
+		rotations = atan2(directionY, directionX) - rotation + PI/2
+		rotate(rotations)
 	
 	Moving = Vector2(directionX, directionY).normalized()
 	velocity = Moving * SPEED
