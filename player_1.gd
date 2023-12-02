4c5f37748a2e5f89ce5c1000ead6ed6f5d8d003c
aymerick <aymerick.soual@epitech.eu>
2023-12-02 18:09:34 +0100

    Adding shouting for player 1

--- player_1.gd
+++ player_1.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
-
-const SPEED = 750.0
+const bullet = preload("res://bullet.tscn")
+const SPEED = 50000.0
 var sprite:Sprite2D
 var posX = 0
 var posY = 0
@@ -17,6 +17,17 @@ func _ready():
 	posX = 1024 - (sprite.texture.get_width() + 1) / 2 - 1
 	posY = 1024 - (sprite.texture.get_height() + 1) / 2 - 1
 
+func spawn_bullet(delta):
+	var curr_b = bullet.instantiate()
+	get_parent().add_child(curr_b)
+	curr_b.position = position
+	curr_b.initi(Vector2.from_angle(rotation - PI / 2), delta)
+	curr_b.rotate(rotation - PI / 2)
+
+func _shoot(_delta):
+	if Input.is_action_just_pressed("ShootP1"):
+		spawn_bullet(_delta)
+
 func _physics_process(_delta):
 	var directionX = Input.get_axis("MoveLeftP1", "MoveRightP1")
 	var directionY = Input.get_axis("MoveUpP1", "MoveDownP1")
@@ -27,5 +38,7 @@ func _physics_process(_delta):
 		rotate(rotations)
 	
 	Moving = Vector2(directionX, directionY).normalized()
-	velocity = Moving * SPEED
+	velocity = Moving * SPEED * _delta
+	_shoot((_delta))
 	move_and_slide()
+
